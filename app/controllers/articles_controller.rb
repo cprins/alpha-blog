4bbe89fc4eb936af129e5b1893b7871394d0323a
Rails Udemi Course <cesarprins@gmail.com>
2017-03-20 02:28:26 +0000

    Se crearon crearon las opciones para editar,modificar, listar y mostrar articulos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,10 @@
 class ArticlesController < ApplicationController
+  
+  def index
+    # variable de entorno para listar todos los articulos
+    @articulos = Article.all
+  end
+  
   def new
     @articulo = Article.new
   end 
@@ -6,13 +12,42 @@ class ArticlesController < ApplicationController
   def create
     # render plain: params[:article].inspect  #:article es el nombre del modelo creado, no el nombre de la variable de entorno
     @articulo = Article.new(article_params)
-    @articulo.save
-    redirect_to articles_show(@articulo)
+    if @articulo.save
+      flash[:notice] = "El articulo fue creado satisfactoriamente"
+      redirect_to article_path(@articulo)
+    else
+      render 'new'
+    end
     
   end 
   
+  def show
+    @articulo = Article.find(params[:id])
+    @article = Article.find(params[:id])
+  end
+  
+  def edit
+    @articulo = Article.find(params[:id])
+  end
+  
+  def update
+    
+    @articulo = Article.find(params[:id])
+    if @articulo.update(article_params)
+      flash[:notice] = "El articulo fue modificado satisfactoriamente"
+      redirect_to article_path(@articulo)
+    else
+      render 'edit'
+    end
+  end
+  
+ 
+  private
+  
   def article_params
+    # son los parametros del modelo creado, actua como un record
     params.require(:article).permit(:title,:description)
   end
+  
 
 end
