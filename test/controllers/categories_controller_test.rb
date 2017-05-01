806925960edd1e48a1cca588ac240ca908662de9
Rails Udemi Course <cesarprins@gmail.com>
2017-05-01 19:35:07 +0000

    Se agrego la funcionalidad de administrador para las categorias

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -5,6 +5,8 @@ class CategoriesControllerTest < ActionController::TestCase
   def setup
     # create en vez de new, ya que el create hace commit de una vez
     @categoria = Category.create( name: "sports")
+    @usuario = User.create(username: "john", email: "john@example.com", password: "password", 
+                           admin: true)
   end
   
   test "debe llegar al index de categorias" do
@@ -13,6 +15,7 @@ class CategoriesControllerTest < ActionController::TestCase
   end
   
   test "debe llegar a la pagina new" do
+    session[:user_id] = @usuario.id
     get :new 
     assert_response :success
   end
@@ -20,6 +23,15 @@ class CategoriesControllerTest < ActionController::TestCase
   test "debe llegar a la pagina show" do
     get(:show, {'id' => @categoria.id})  
     assert_response :success
+  end
+  
+  test "should redirect create when admin not logged in" do
+    # no debe haber cambios en la cantidad de categorias al realizar la accion dentro del DO
+    assert_no_difference 'Category.count' do
+      post :create, category: { name: "sports" }
+    end
+    # redirecciona al listado de categorias
+    assert_redirected_to categories_path
   end
   
 end
